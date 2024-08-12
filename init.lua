a3d3c07e1669c2cc1e9eb968fccde4448192a9ce
cawthm <wmc@MacBook-Pro-10.attlocal.net>
2024-08-11 19:08:18 -0500

    added r_language server to init.lua

--- init.lua
+++ init.lua
@@ -573,6 +573,7 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
+        r_language_server = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
